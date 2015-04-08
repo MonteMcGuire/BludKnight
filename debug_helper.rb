6a0c21b48cdc289d5cb2345edf5fa2c2acba174d
Monte McGuire <montem@hightouchinc.com>
2015-04-08 16:45:27 -0500

    Initial creation
    Debug tracer module, I use for everything.

---
+++ debug_helper.rb
@@ -0,0 +1,22 @@
+# DebugHelper
+module DebugHelper
+  def debug_helper_true(f1, s2)
+    private_debug_helper(f1, s2, true)
+  end
+
+  def debug_helper_false(f1, s2)
+    private_debug_helper(f1, s2, false)
+  end
+
+  private
+
+  def private_debug_helper(f1, s2, line_doit)
+    return unless line_doit
+    mmm_dd = Time.new.strftime('%b_%d')
+    f = File.open("/tmp/monte_#{mmm_dd}.log", 'a')
+    time_format = Time.new.strftime('%m/%d/%y %H:%M:%S')
+    file = File.basename(f1) + '                         '
+    f.puts "#{time_format} #{file[0..19]}, #{s2}"
+    f.close
+  end
+end
